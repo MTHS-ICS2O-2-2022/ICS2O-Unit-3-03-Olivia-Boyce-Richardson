63149a7a112b44de77246d7f6d975f02ba1d6746
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-11 17:25:57 +0000

    js fixes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function calculate () {
+  // input
+  const radius = parseInt(document.getElementById('radius').value)
+
+  // process
+  const volume = (4/3) * Math.PI * radius**3
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + '.'
+}
